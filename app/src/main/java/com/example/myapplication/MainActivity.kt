c2e07c663530eda62a37d3b90e4e54fb5c24274a
Udoh Idorenyin <idorenyinudoh@users.noreply.github.com>
2020-08-12 15:41:49 +0100

    code to add

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_plus.setOnClickListener {
+            var sum : Double = number1.toString().toDouble() + number2.toString().toDouble()
+            result_view.text = sum.toString()
+        }
+
     }
 }
